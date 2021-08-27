c18f84f45264cbfaa2b2a47e691d6abd7f5c3bf3
Andrey <sudovae03@mail.ru>
2021-08-27 20:14:31 +0300

    Renew models.py

--- django/analyst_service/analyst/admin.py
+++ django/analyst_service/analyst/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('name', 'address', 'address_lng', 'gender', 'age')
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'text')
+
+
+@admin.register(Like)
+class LikeAdmin(admin.ModelAdmin):
+    list_display = ('date', 'user', 'post')
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = ('date', 'place', 'sum')
+
+
+# @admin.register(Quotes)
+# class QuotesAdmin(admin.ModelAdmin):
+#     list_display = ('pair', 'quote', 'is_full')
+#
+# @admin.register(Profile)
+# class ProfileAdmin(admin.ModelAdmin):
+#     list_display = ('external_id', 'name', 'numQuestionProfile', 'typeOfProfile')
--- django/analyst_service/analyst/models.py
+++ django/analyst_service/analyst/models.py
@@ -1,8 +1,39 @@
 from django.db import models
 
 
+class User(models.Model):
+    name = models.TextField(verbose_name='Имя')
+    address = models.TextField(verbose_name='адрес')
+    address_lng = models.TextField(verbose_name='адрес(Кординаты)')
+    gender = models.TextField(verbose_name='Пол')
+    age = models.PositiveIntegerField(verbose_name='Возраст')
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+
+
+class Post(models.Model):
+    title = models.TextField('Заголовок')
+    text = models.TextField('Текст')
+
+    class Meta:
+        verbose_name = 'Пост'
+        verbose_name_plural = 'Посты'
+
+
+class Like(models.Model):
+    date = models.DateTimeField(verbose_name='Дата', auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Лайк'
+        verbose_name_plural = 'Лайки'
+
+
 class Transaction(models.Model):
-    date = models.DateTimeField(verbose_name='Дата транзакции')
+    date = models.DateTimeField(verbose_name='Дата', auto_now_add=True)
     place = models.TextField(verbose_name='Место')
     sum = models.FloatField(verbose_name='Сумма')
 
@@ -10,10 +41,3 @@ class Transaction(models.Model):
         verbose_name = 'Транзакция'
         verbose_name_plural = 'Транзакции'
 
-
-class User(models.Model):
-    address = models.TextField(verbose_name='адрес')
-
-    class Meta:
-        verbose_name = 'Пользователь'
-        verbose_name_plural = 'Пользователи'
